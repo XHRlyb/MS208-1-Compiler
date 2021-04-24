0bedb83bbe1c8edc58b7bad302d34e928cf9e320
XHRlyb <36755791+XHRlyb@users.noreply.github.com>
2021-04-24 14:39:14 +0800

    op2 adce
    try adce

--- src/Optim/Optim.java
+++ src/Optim/Optim.java
@@ -9,10 +9,12 @@ public class Optim {
     public Optim(IR ir) { this.ir = ir; }
 
     public void work() {
-//        for (int i = 0; i < 10; i++)  {
+        for (int i = 0; i < 10; i++)  {
             new CleanUp(ir).work();
             new SCCP(ir).work();
-//        }
+            new CleanUp(ir).work();
+            new ADCE(ir).work();
+        }
         //new IRPrinter(System.out, ir).outp();
         new CleanUp(ir).work();
     }
